56fbe2810decab5e45b463dfa6f00ad505ba99ee
onsuk <ondaesuk93@gmail.com>
2019-07-27 19:41:11 +0900

    Add: bounce mail handler

---
+++ src/database/models/subscriber.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+import Joi from '@hapi/joi'
+
+const SubscriberSchema = new Schema({
+  createdAt: { type: Date, default: Date.now },
+  email: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  isCertify: {
+    type: Boolean,
+    required: true,
+  },
+  favoriteTags: [{ type: String }],
+  received: [{ type: Schema.Types.ObjectId, ref: 'Interview' }],
+});
+
+export default mongoose.model('Subscriber', SubscriberSchema);
